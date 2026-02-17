bc7a47df025feb95a74c6ffd372baf4e3dabcc2b
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-17 12:54:21 +0100

    tipping algorithm

--- day2/main.py
+++ day2/main.py
@@ -1,2 +1,9 @@
-def hello():
- print("Hello")
+print("Welcome to the tip calculator")
+total_bill = int(input("what was the total bill? $"))
+tip_percent = int(
+    input("How much tip would you like to give? 10, 12, or 15? "))
+no_people = int(input("How many people to split the bill? "))
+
+each_person_amount = total_bill * (1 + tip_percent/100) / no_people
+
+print(f"Each Person should pay: ${each_person_amount:.2f}")
