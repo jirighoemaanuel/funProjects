74e4e57f7957235aab1241548cacf517fdce7d5d
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-18 23:26:06 +0100

    done

---
+++ day7/main.py
@@ -0,0 +1,57 @@
+import random
+
+HANGMANPICS = ['''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+   =========''', '''
+    +---+
+    |   |
+    o   |
+        |
+        |
+        |
+   =========''', '''
+    +---+
+    |   |
+    o   |
+    |   |
+        |
+        |
+   =========''', '''
+    +---+
+    |   |
+    o   |
+   /|   |
+        |
+        |
+   =========''', '''
+    +---+
+    |   |
+    o   |
+   /|\  |
+        |
+        |
+   =========''', '''
+    +---+
+    |   |
+    o   |
+   /|\  |
+   /    |
+        |
+   ========''', '''
+    +---+
+    |   |
+    o   |
+   /|\  |
+   / \  |
+        |
+   ========''']
+
+words = "ape cat dog baboon elephant giraffe apple coconut monkey rubik mice mouse pineapple android apple house fence python grail zerg protoss terran llama fire policeman mamerto smad zebra lion luffy nigga universidad".split()
+
+def hangman():
+     pass
