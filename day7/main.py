7bd5658d48e1671f9065abe6ca372768084dc3ed
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-19 19:44:46 +0100

    still figuring it out

--- day7/main.py
+++ day7/main.py
@@ -51,7 +51,46 @@ HANGMANPICS = ['''
         |
    ========''']
 
-words = "ape cat dog baboon elephant giraffe apple coconut monkey rubik mice mouse pineapple android apple house fence python grail zerg protoss terran llama fire policeman mamerto smad zebra lion luffy nigga universidad".split()
+print(""" _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                 """)
 
-def hangman():
-     pass
+
+words = "ape cat dog baboon elephant giraffe apple coconut monkey rubik mice mouse pineapple android apple house fence python grail zerg protoss terran llama fire policeman mamerto smad zebra lion luffy  universidad".split()
+
+word = random.choice(words)
+
+"""
+1. word to guess
+2. guess
+3. You guessed 'letter', that's not in the word, you lose a life,
+4. track life. 
+5. You guess 
+4. if a letter is guessed in a word, remove it from the word
+"""
+
+counter = 0
+
+def display(word):
+    word_dash = ""
+    for _ in range(len(word)):
+        word_dash += "_"
+
+    print(f"Word to guess: {word_dash}")
+
+
+def hangman(guess, word):
+    if guess in word:
+        return True
+    else:
+        return False
+
+
+while counter < 2:
+    display(word)
+    counter += 1
