3845ef1cb880f0477d03ac7b2aed3826f12b0bbd
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-20 02:08:03 +0100

    done day8

--- day8/main.py
+++ day8/main.py
@@ -1,18 +1,37 @@
 alphabets = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrpt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 def encrypt(original_text, shift_amount):
     encrypted_wrd = ""
+    length_alphabets = len(alphabets)
     for letter in original_text:
         new_index = alphabets.index(letter) + shift_amount
-        encrypted_wrd += alphabets[new_index]
+        encrypted_wrd += alphabets[new_index % length_alphabets]
 
-    return encrypted_wrd
+    print(encrypted_wrd)
 
 
-print(encrypt(text, shift))
+def decrypt(original_text, shift_amount):
+    decrypted_wrd = ""
+    length_alphabets = len(alphabets)
+    for letter in original_text:
+        new_index = alphabets.index(letter) - shift_amount
+        decrypted_wrd += alphabets[new_index % length_alphabets]
+
+    print(decrypted_wrd)
+
+
+def caeser(direction, original_text, shift_amount):
+    if direction == "encode":
+        encrypt(original_text, shift_amount)
+    elif direction == "decode":
+        decrypt(original_text, shift_amount)
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrpt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(direction, text, shift)
+
+    
