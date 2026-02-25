55ff16845c71228e8dc50f35a0c7bb81cba021ac
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-25 23:28:19 +0100

    still on it

--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -2,11 +2,9 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
-        self.next_question()
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        answer = input(f"{current_question.text}?:")
+        answer = input(
+            f"Q.{self.question_number}: {current_question.text}? (True/False): ")
         return answer
-
-    next_question()
