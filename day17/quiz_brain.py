1fc6952e68f4e85b0edefacbcc063534c1d4c425
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-25 23:18:29 +0100

    still working on logic

--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -2,8 +2,11 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
-    
+        self.next_question()
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         answer = input(f"{current_question.text}?:")
         return answer
+
+    next_question()
