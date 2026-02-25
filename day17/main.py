4f902d72580ea3face4fe1720dd17f2fb535c164
jirighoemaanuel <emmaweyinmi@gmail.com>
2026-02-26 00:10:14 +0100

    done

--- day17/main.py
+++ day17/main.py
@@ -12,4 +12,6 @@ for question in question_data:
     question_bank.append(new_question)
 
 
-question = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
